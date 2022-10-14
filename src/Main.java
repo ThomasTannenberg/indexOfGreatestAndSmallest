c32d10bab117e9798b42283f359194ddbdc08f6c
Thomas <Thomas@Tannenberg.online>
2022-10-14 07:32:04 +0200

    imported utilitis

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+
+
+
+
+
+    }
+}
